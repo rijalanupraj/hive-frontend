8d3a17a97f85a2dd45eee2bcc5b5a9d31b0e981b
Shankar4919 <itsme.shankar8@gmail.com>
2022-05-13 19:28:30 +0545

    user detail card

--- frontend/src/component/User/UserProfile.js
+++ frontend/src/component/User/UserProfile.js
@@ -22,6 +22,32 @@ const UserProfile = () => {
                     <button type="button" className="btn btn-success btn-sm"><i className="fa fa-check"></i> Follow</button>
                     <button type="button" className="btn btn-primary btn-sm"><i className="fa fa-envelope"></i> Send Message</button>
                 </div>
+
+            </div>
+            <div className='buttonNav'>
+                <p>hello</p>
+            </div>
+            {/* User Card Info */}
+           
+            <div class="col-md-4 mb-3">
+              <div class="usercard">
+
+                <div className="aboutUser">
+                    <p className='aboutuserpara'>About User</p>
+                </div>
+                <hr className="border-light m-0"/>
+
+                <div className="aboutUserDetailcard">
+                    <p>I help needy people, after being paid</p>
+                    <p>Follower: 1B</p>     
+                </div>
+                    <hr className="border-light m-0"/>
+                <div className="card-body">
+                    <p className="mb-2">Name: Meydhi Vai</p>
+                    <p className="mb-2">Sector: Krishne Basantay</p>
+                    <p>Created Dec 12 0101</p>
+                </div>
+                </div>
             </div>
             
         </div>
