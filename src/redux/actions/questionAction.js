a7330412ecec9202472b8a79121ce2230ca1723f
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-05-20 14:48:05 +0545

    ⚡️ Fix api bug

--- src/redux/actions/questionAction.js
+++ src/redux/actions/questionAction.js
@@ -1,13 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-import { attachTokenToHeaders } from "./authActions";
-import {
-  ASKQUESTION_LOADING,
-  ASKQUESTION_SUCCESS,
-  ASKQUESTION_FAIL,
-} from "../types";
+import { ASKQUESTION_LOADING, ASKQUESTION_SUCCESS, ASKQUESTION_FAIL } from '../types';
 
-const API_URL = "http://localhost:8000/api/v1";
+const API_URL = 'http://localhost:8000/api/v1';
 
 // export const askQuestion =
 //   (formData, navigate) => async (dispatch, getState) => {
@@ -27,26 +22,22 @@ const API_URL = "http://localhost:8000/api/v1";
 //     }
 //   };
 
-export const askQuestion =
-  (formData, navigate) => async (dispatch, getState) => {
-    try {
-      dispatch({ type: ASKQUESTION_LOADING });
-      const options = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
+export const askQuestion = (formData, navigate) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ASKQUESTION_LOADING });
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': localStorage.getItem('token')
+      }
+    };
 
-      const { data } = await axios.post(
-        `${API_URL}/question`,
-        formData,
-        options
-      );
-      dispatch({ type: ASKQUESTION_SUCCESS, payload: data });
-    } catch (err) {
-      dispatch({
-        type: ASKQUESTION_FAIL,
-        payload: { error: err?.response?.data.message || err.message },
-      });
-    }
-  };
+    const { data } = await axios.post(`${API_URL}/question`, formData, options);
+    dispatch({ type: ASKQUESTION_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: ASKQUESTION_FAIL,
+      payload: { error: err?.response?.data.message || err.message }
+    });
+  }
+};
--- src/userpages/AskQuestion/askque.js
+++ src/userpages/AskQuestion/askque.js
@@ -1,81 +1,77 @@
-import { React, useState, useEffect } from "react";
-import * as Yup from "yup";
+import { React, useState, useEffect } from 'react';
+import * as Yup from 'yup';
 
-import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Stack, TextField } from "@mui/material";
-import { useFormik, Form, FormikProvider } from "formik";
-import { LoadingButton } from "@mui/lab";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { askQuestion } from "../../redux/actions/questionAction";
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Stack, TextField } from '@mui/material';
+import { useFormik, Form, FormikProvider } from 'formik';
+import { LoadingButton } from '@mui/lab';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { askQuestion } from '../../redux/actions/questionAction';
 
 const theme = createTheme();
 
-const category = ["Government", "Health", "Education", "Vechiles"];
+const category = ['Government', 'Health', 'Education', 'Vechiles'];
 const questiontag = [
-  { title: "samadhan" },
-  { title: "school" },
-  { title: "college" },
-  { title: "government" },
-  { title: "hospital" },
-  { title: "travel" },
-  { title: "rules" },
-  { title: "Relationship" },
-  { title: "law" },
+  { title: 'samadhan' },
+  { title: 'school' },
+  { title: 'college' },
+  { title: 'government' },
+  { title: 'hospital' },
+  { title: 'travel' },
+  { title: 'rules' },
+  { title: 'Relationship' },
+  { title: 'law' }
 ];
 
 export default function AskQuestion() {
   const dispatch = useDispatch();
-  const question = useSelector((state) => state.question);
+  const question = useSelector(state => state.question);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (question.error) {
       formik.setSubmitting(false);
     }
+    if (question.question) {
+      formik.setSubmitting(false);
+    }
   }, [question.error]);
 
   const QuestionSchema = Yup.object().shape({
     title: Yup.string()
-      .min(5, "Too Short!")
-      .max(70, "Too Long!")
-      .required("Please choose an appropriate title for your question"),
-    category: Yup.string().required("Category required"),
-    tags: Yup.array().required("Tags required"),
+      .min(5, 'Too Short!')
+      .max(70, 'Too Long!')
+      .required('Please choose an appropriate title for your question'),
+    category: Yup.string().required('Category required'),
+    tags: Yup.array().required('Tags required'),
     description: Yup.string()
-      .min(6, "Too short")
-      .required("Please write down descriptio for your question"),
   });
   const formik = useFormik({
     initialValues: {
-      title: "",
-      category: "",
+      title: '',
+      category: '',
       tags: [],
-      description: "",
+      description: ''
     },
     validationSchema: QuestionSchema,
     onSubmit: () => {
-      dispatch(askQuestion(formik.values, navigate));
-    },
+      const dataValues = {
+        ...formik.values,
+        tags: formik.values.tags.map(tag => tag.title)
+      };
+      dispatch(askQuestion(dataValues, navigate));
+    }
   });
-  const {
-    errors,
-    touched,
-    handleSubmit,
-    isSubmitting,
-    getFieldProps,
-    setFieldValue,
-  } = formik;
-  const [value, setValue] = useState(category[-1]);
-  const [inputValue, setInputValue] = useState("");
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps, setFieldValue } = formik;
 
   useEffect(() => {
     console.log(touched);
@@ -85,31 +81,27 @@ export default function AskQuestion() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper
-          variant="outlined"
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-        >
+      <Container component='main' maxWidth='sm' sx={{ mb: 4 }}>
+        <Paper variant='outlined' sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography
-            component="div"
-            variant="body1"
+            component='div'
+            variant='body1'
             style={{
               height: 40,
-              width: "100%",
-              position: "relative",
+              width: '100%',
+              position: 'relative'
             }}
           >
             <Stack
               sx={{
-                color: (theme) =>
-                  theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+                color: theme => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
 
-                fontSize: "1rem",
-                fontWeight: "700",
-                position: "absolute",
+                fontSize: '1rem',
+                fontWeight: '700',
+                position: 'absolute',
                 top: 0,
                 left: 0,
-                zIndex: "modal",
+                zIndex: 'modal'
               }}
             >
               Create New Question
@@ -117,16 +109,16 @@ export default function AskQuestion() {
           </Typography>
 
           <FormikProvider value={formik}>
-            <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <Form autoComplete='off' noValidate onSubmit={handleSubmit}>
               <Stack spacing={3}>
-                <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
                   <TextField
                     fullWidth
-                    label="Title"
-                    type="text"
+                    label='Title'
+                    type='text'
                     required
-                    placeholder="Enter your title here "
-                    {...getFieldProps("title")}
+                    placeholder='Enter your title here '
+                    {...getFieldProps('title')}
                     error={Boolean(touched.title && errors.title)}
                     helperText={touched.title && errors.title}
                   />
@@ -160,35 +152,34 @@ export default function AskQuestion() {
                     )}
                   />
                 </Stack> */}
-                <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
                   <Autocomplete
                     fullWidth={true}
                     style={{
-                      width: "100%",
-                      fullWidth: true,
+                      width: '100%',
+                      fullWidth: true
                     }}
                     required
-                    {...getFieldProps("category")}
+                    {...getFieldProps('category')}
                     onChange={(event, value) => {
-                      setFieldValue("category", value);
+                      setFieldValue('category', value);
                     }}
+                    defaultValue={category[0]}
                     error={Boolean(touched.category && errors.category)}
                     helperText={touched.category && errors.category}
                     options={category}
-                    isOptionEqualToValue={(option, value) =>
-                      option.title === value.title
-                    }
+                    isOptionEqualToValue={(option, value) => option.title === value.title}
                     sx={{ width: 300 }}
-                    renderInput={(params) => (
+                    renderInput={params => (
                       <TextField
                         {...params}
-                        label="Category"
-                        placeholder="search or choose category"
+                        label='Category'
+                        placeholder='search or choose category'
                       />
                     )}
                   />
                   {touched.category && errors.category ? (
-                    <div className="error">{errors.category}</div>
+                    <div className='error'>{errors.category}</div>
                   ) : null}
                 </Stack>
 
@@ -215,30 +206,24 @@ export default function AskQuestion() {
                     )}
                   />
                 </Stack> */}
-                <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
                   <Autocomplete
                     multiple
-                    id="tags-outlined"
+                    id='tags-outlined'
                     options={questiontag}
                     style={{
-                      width: "100%",
-                      fullWidth: true,
+                      width: '100%',
+                      fullWidth: true
                     }}
-                    getOptionLabel={(option) => option.title}
+                    getOptionLabel={option => option.title}
                     defaultValue={[questiontag[0]]}
                     onChange={(event, value) => {
                       // UPdate Array of tags
-                      setFieldValue("tags", value);
+                      setFieldValue('tags', value);
                     }}
-                    isOptionEqualToValue={(option, value) =>
-                      option.title === value.title
-                    }
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        label="Tags"
-                        placeholder="search or choose tags"
-                      />
+                    isOptionEqualToValue={(option, value) => option.title === value.title}
+                    renderInput={params => (
+                      <TextField {...params} label='Tags' placeholder='search or choose tags' />
                     )}
                   />
                 </Stack>
@@ -256,38 +241,38 @@ export default function AskQuestion() {
                     helperText={touched.description && errors.description}
                   />
                 </Stack> */}
-                <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
                   <TextareaAutosize
-                    label="Description"
+                    label='Description'
                     style={{
-                      width: "100%",
-                      fullWidth: true,
+                      width: '100%',
+                      fullWidth: true
                     }}
                     minRows={4}
                     required
-                    placeholder="Description"
-                    {...getFieldProps("description")}
+                    placeholder='Description'
+                    {...getFieldProps('description')}
                     error={Boolean(touched.description && errors.description)}
                     helperText={touched.description && errors.description}
                   />
                   {touched.description && errors.description ? (
-                    <div className="error">{errors.description}</div>
+                    <div className='error'>{errors.description}</div>
                   ) : null}
                 </Stack>
                 <Grid container>
                   <Grid item xs>
-                    <Button variant="outlined" color="error">
+                    <Button variant='outlined' color='error'>
                       Cancel
                     </Button>
                   </Grid>
 
                   <LoadingButton
                     xs={1}
-                    size="large"
-                    type="submit"
-                    justifycontent="flex-end"
-                    color="success"
-                    variant="contained"
+                    size='large'
+                    type='submit'
+                    justifycontent='flex-end'
+                    color='success'
+                    variant='contained'
                     loading={isSubmitting}
                   >
                     Submit
