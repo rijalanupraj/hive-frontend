a0a2cc8d93e0ebc3f9644e9ef81ddb0bb5a0d079
samirthapaliya00 <43093117+samirthapaliya@users.noreply.github.com>
2022-05-22 08:31:09 +0545

    update user profile

--- src/userpages/UpdateUserProfile/UpdateUserProfile.js
+++ src/userpages/UpdateUserProfile/UpdateUserProfile.js
@@ -1,56 +1,56 @@
-import React from 'react';
+import React from "react";
 
-import UpdateProfileDetail from './components/Profile';
-import UpdateProfileSecurity from './components/Security';
+import UpdateProfileDetail from "./components/Profile";
+import UpdateProfileSecurity from "./components/Security";
 
-import './css/updateUserProfile.css';
+import "./css/updateUserProfile.css";
 const UpdateUserProfile = () => {
   return (
-    <div class='container-xl px-4 mt-4'>
-      <ul class='nav nav-pills mb-3' id='pills-tab' role='tablist'>
-        <li class='nav-item' role='presentation'>
+    <div class="container-xl px-4 mt-4">
+      <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li class="nav-item" role="presentation">
           <button
-            class='nav-link active'
-            id='pills-home-tab'
-            data-bs-toggle='pill'
-            data-bs-target='#pills-home'
-            type='button'
-            role='tab'
-            aria-controls='pills-home'
-            aria-selected='true'
+            class="nav-link active"
+            id="pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-home"
+            type="button"
+            role="tab"
+            aria-controls="pills-home"
+            aria-selected="true"
           >
             Profile
           </button>
         </li>
-        <li class='nav-item' role='presentation'>
+        <li class="nav-item" role="presentation">
           <button
-            class='nav-link'
-            id='pills-profile-tab'
-            data-bs-toggle='pill'
-            data-bs-target='#pills-profile'
-            type='button'
-            role='tab'
-            aria-controls='pills-profile'
-            aria-selected='false'
+            class="nav-link"
+            id="pills-profile-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-profile"
+            type="button"
+            role="tab"
+            aria-controls="pills-profile"
+            aria-selected="false"
           >
             Setting
           </button>
         </li>
       </ul>
-      <div class='tab-content' id='pills-tabContent'>
+      <div class="tab-content" id="pills-tabContent">
         <div
-          class='tab-pane fade show active'
-          id='pills-home'
-          role='tabpanel'
-          aria-labelledby='pills-home-tab'
+          class="tab-pane fade show active"
+          id="pills-home"
+          role="tabpanel"
+          aria-labelledby="pills-home-tab"
         >
           <UpdateProfileDetail />
         </div>
         <div
-          class='tab-pane fade'
-          id='pills-profile'
-          role='tabpanel'
-          aria-labelledby='pills-profile-tab'
+          class="tab-pane fade"
+          id="pills-profile"
+          role="tabpanel"
+          aria-labelledby="pills-profile-tab"
         >
           <UpdateProfileSecurity />
         </div>
