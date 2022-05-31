6fe286a11072d79c7f49f3d96ea9599d3ce8c00c
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-05-31 14:55:38 +0545

    ✨ Add Upvote & DownVote in view solution page

--- src/redux/actions/viewSolutionActions.js
+++ src/redux/actions/viewSolutionActions.js
@@ -1,30 +1,87 @@
-import axios from "axios";
+import axios from 'axios';
 
 //internal import
-import * as TYPES from "../types";
-import { BACKEND_API_URL } from "../../constants";
+import * as TYPES from '../types';
+import { BACKEND_API_URL } from '../../constants';
 
 const API_URL = BACKEND_API_URL;
 
-export const viewSolution = (solutionId) => async (dispatch, getState) => {
+export const viewSolution = solutionId => async (dispatch, getState) => {
   try {
     dispatch({ type: TYPES.VIEW_SOLUTION_LOADING });
 
-    const response = await axios.get(
-      `${API_URL}/solution/${solutionId}`,
-      
-    );
+    const response = await axios.get(`${API_URL}/solution/${solutionId}`);
 
     dispatch({
       type: TYPES.VIEW_SOLUTION_SUCCESS,
-      payload: response.data,
+      payload: response.data
     });
   } catch (err) {
     dispatch({
       type: TYPES.VIEW_SOLUTION_FAIL,
       payload: {
-        error: err?.response?.data.message || err.message,
-      },
+        error: err?.response?.data.message || err.message
+      }
     });
   }
+};
+
+export const upVoteSolution = solutionId => async (dispatch, getState) => {
+  try {
+    dispatch({ type: TYPES.VIEW_SOLUTION_LOADING });
+    const options = attachTokenToHeaders(getState);
+    const response = await axios.post(`${API_URL}/solution/upvote/${solutionId}`, {}, options);
+
+    dispatch({
+      type: TYPES.UPVOTE_SOLUTION_SUCCESS,
+      payload: {
+        updatedSolution: response.data.updatedPost
+      }
+    });
+  } catch (err) {
+    dispatch({
+      type: TYPES.UPVOTE_SOLUTION_FAIL,
+      payload: {
+        error: err?.response?.data.message || err.message
+      }
+    });
+  }
+};
+
+export const downVoteSolution = solutionId => async (dispatch, getState) => {
+  try {
+    const options = attachTokenToHeaders(getState);
+    dispatch({ type: TYPES.VIEW_SOLUTION_LOADING });
+    const response = await axios.post(`${API_URL}/solution/downvote/${solutionId}`, {}, options);
+
+    dispatch({
+      type: TYPES.DOWNVOTE_SOLUTION_SUCCESS,
+      payload: {
+        updatedSolution: response.data.updatedPost
+      }
+    });
+  } catch (err) {
+    dispatch({
+      type: TYPES.DOWNVOTE_SOLUTION_FAIL,
+      payload: {
+        error: err?.response?.data.message || err.message
+      }
+    });
+  }
+};
+
+export const attachTokenToHeaders = getState => {
+  const token = getState().auth.token;
+
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  };
+
+  if (token) {
+    config.headers['x-auth-token'] = token;
+  }
+
+  return config;
 };
--- src/userpages/ViewSolution/ViewSolution.js
+++ src/userpages/ViewSolution/ViewSolution.js
@@ -1,38 +1,45 @@
-import React, { useState, useEffect } from "react";
-import * as Yup from "yup";
-import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Stack, TextField } from "@mui/material";
-import { useFormik, Form, FormikProvider } from "formik";
-import { LoadingButton } from "@mui/lab";
-import IconButton from "@mui/material/IconButton";
-import Box from "@mui/material/Box";
-import Chip from "@mui/material/Chip";
-import Avatar from "@mui/material/Avatar";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import { viewSolution } from "../../redux/actions/viewSolutionActions";
+import React, { useState, useEffect } from 'react';
+import * as Yup from 'yup';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Stack, TextField } from '@mui/material';
+import { useFormik, Form, FormikProvider } from 'formik';
+import { LoadingButton } from '@mui/lab';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Avatar from '@mui/material/Avatar';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import {
+  viewSolution,
+  upVoteSolution,
+  downVoteSolution
+} from '../../redux/actions/viewSolutionActions';
 
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 const theme = createTheme();
 export default function AskQuestion() {
   const dispatch = useDispatch();
   const { solutionId } = useParams();
-  const solution = useSelector((state) => state.viewSolutions);
+  const solution = useSelector(state => state.viewSolutions);
+  const auth = useSelector(state => state.auth);
+  const navigate = useNavigate();
+
   useEffect(() => {
     dispatch(viewSolution(solutionId));
   }, []);
 
   const [expanded, setExpanded] = React.useState(false);
 
-  const handleChange = (panel) => (event, isExpanded) => {
+  const handleChange = panel => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
 
@@ -43,51 +50,44 @@ export default function AskQuestion() {
       <Stack spacing={3}>
         <Box
           sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            "& > :not(style)": {
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
               m: 0,
-              width: "100%",
-              height: "10%",
-            },
+              width: '100%',
+              height: '10%'
+            }
           }}
         >
           <Paper elevation={0} />
           <Box
-            component="img"
-            alt="The house from the offer."
-            src="https://i.ibb.co/grnLWrt/cover.png"
+            component='img'
+            alt='The house from the offer.'
+            src='https://i.ibb.co/grnLWrt/cover.png'
           />
           <Paper />
         </Box>
         <Stack
           sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            "& > :not(style)": {
-              mx: 5,
-            },
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              mx: 5
+            }
           }}
         >
-          <Typography
-            variant="h6"
-            sx={{ fontWeight: "bold" }}
-            component="div"
-            gutterBottom
-          >
+          <Typography variant='h6' sx={{ fontWeight: 'bold' }} component='div' gutterBottom>
             {solution?.solution?.question?.title}
           </Typography>
           <Typography
-            sx={{ fontWeight: "bold", typography: "body2", mx: 6 }}
-            component="div"
+            sx={{ fontWeight: 'bold', typography: 'body2', mx: 6 }}
+            component='div'
             gutterBottom
           >
             {solution?.solution?.question?.category}
             <Chip
-              label={`posted on: ${
-                solution?.solution?.question?.createdAt.split("T")[0]
-              }`}
-              variant="outlined"
+              label={`posted on: ${solution?.solution?.question?.createdAt.split('T')[0]}`}
+              variant='outlined'
               sx={{ mx: 4 }}
             />
           </Typography>
@@ -100,65 +100,100 @@ export default function AskQuestion() {
             <Avatar
               sx={{}}
               src={solution?.solution?.user?.profilePhoto}
-              aria-label="recipe"
+              aria-label='recipe'
             ></Avatar>
           }
           title={solution?.solution?.user?.username}
           subheader={`followers: ${solution?.solution?.user?.followers.length}`}
         />
       </Card>
-      <Card sx={{ maxWidth: 150, mx: 5, my: 3, borderRadius: 4 }}>
-        <Box sx={{ display: "flex", alignItems: "center", pl: 2, pb: 0 }}>
-          <IconButton style={{ color: "green", fontWeight: 25, fontSize: 20 }}>
-            <ArrowUpwardIcon />
-            {solution?.solution?.upVotes.length}
-          </IconButton>
+      {auth.isAuthenticated && (
+        <Card sx={{ maxWidth: 150, mx: 5, my: 3, borderRadius: 4 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', pl: 2, pb: 0 }}>
+            <IconButton
+              style={{ color: 'green', fontWeight: 25, fontSize: 20 }}
+              onClick={() => dispatch(upVoteSolution(solution?.solution?._id))}
+            >
+              <ArrowUpwardIcon />
+              {solution?.solution?.upVotes.length}
+            </IconButton>
 
-          <IconButton style={{ color: "red", fontWeight: 25, fontSize: 20 }}>
-            <ArrowDownwardIcon />
-            {solution?.solution?.downVotes.length}
-          </IconButton>
-        </Box>
-      </Card>
+            <IconButton
+              style={{ color: 'red', fontWeight: 25, fontSize: 20 }}
+              onClick={() => dispatch(downVoteSolution(solution?.solution?._id))}
+            >
+              <ArrowDownwardIcon />
+              {solution?.solution?.downVotes.length}
+            </IconButton>
+          </Box>
+        </Card>
+      )}
+
+      {!auth.isAuthenticated && (
+        <Card sx={{ maxWidth: 150, mx: 5, my: 3, borderRadius: 4 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', pl: 2, pb: 0 }}>
+            <IconButton
+              style={{ color: 'green', fontWeight: 25, fontSize: 20 }}
+              onClick={() => {
+                navigate('/login?redirectTo=' + window.location.pathname);
+              }}
+            >
+              <ArrowUpwardIcon />
+              {solution?.solution?.upVotes.length}
+            </IconButton>
+
+            <IconButton
+              style={{ color: 'red', fontWeight: 25, fontSize: 20 }}
+              onClick={() => {
+                navigate('/login?redirectTo=' + window.location.pathname);
+              }}
+            >
+              <ArrowDownwardIcon />
+              {solution?.solution?.downVotes.length}
+            </IconButton>
+          </Box>
+        </Card>
+      )}
+
       <Grid container>
         <Grid item xs={8}>
           <Stack
             sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              "& > :not(style)": {
-                mx: 5,
-              },
+              display: 'flex',
+              flexWrap: 'wrap',
+              '& > :not(style)': {
+                mx: 5
+              }
             }}
           >
             <Typography
-              variant="h6"
-              sx={{ fontWeight: "bold", my: 2 }}
-              component="div"
+              variant='h6'
+              sx={{ fontWeight: 'bold', my: 2 }}
+              component='div'
               gutterBottom
             >
               Introduction
             </Typography>
-            <Typography variant="body2" gutterBottom>
+            <Typography variant='body2' gutterBottom>
               {solution?.solution?.answer}
             </Typography>
             <Typography
-              variant="h6"
-              sx={{ fontWeight: "bold", my: 2 }}
-              component="div"
+              variant='h6'
+              sx={{ fontWeight: 'bold', my: 2 }}
+              component='div'
               gutterBottom
             >
               Solution
             </Typography>
-            <Stack direction="row" spacing={1} sx={{ my: 12 }}>
-              {solution?.solution?.tags.map((tag) => (
-                <Chip label={tag} variant="outlined" />
+            <Stack direction='row' spacing={1} sx={{ my: 12 }}>
+              {solution?.solution?.tags.map(tag => (
+                <Chip label={tag} variant='outlined' />
               ))}
             </Stack>
           </Stack>
         </Grid>
 
-        <Grid item sx={{ justifyContent: "end" }}>
+        <Grid item sx={{ justifyContent: 'end' }}>
           ...asdsssssssssssssssssssssssssss
         </Grid>
       </Grid>
