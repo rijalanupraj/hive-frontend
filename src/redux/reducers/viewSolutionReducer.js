e5721569f0b18ad85dbc69529f7cfbcbbdcd802b
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-05-31 15:38:41 +0545

    ✨ Add Upvote & DownVote in view solution page (#24)

--- src/redux/reducers/viewSolutionReducer.js
+++ src/redux/reducers/viewSolutionReducer.js
@@ -1,31 +1,45 @@
-import * as TYPES from "../types";
+import * as TYPES from '../types';
 
 const initialState = {
   isLoading: false,
-  error: null,
+  error: null
 };
 
-export default function viewSolutionReducer(
-  state = initialState,
-  { type, payload }
-) {
+export default function viewSolutionReducer(state = initialState, { type, payload }) {
   switch (type) {
     case TYPES.VIEW_SOLUTION_LOADING:
       return {
         ...state,
-        isLoading: true,
+        isLoading: true
       };
     case TYPES.VIEW_SOLUTION_SUCCESS:
       return {
         ...state,
         isLoading: false,
-        solution: payload.solution,
+        solution: payload.solution
       };
     case TYPES.VIEW_SOLUTION_FAIL:
       return {
         ...state,
         isLoading: false,
-        error: payload.error,
+        error: payload.error
+      };
+    case TYPES.UPVOTE_SOLUTION_SUCCESS:
+    case TYPES.DOWNVOTE_SOLUTION_SUCCESS:
+      console.log(payload.updatedSolution);
+      return {
+        ...state,
+        solution: {
+          ...state.solution,
+          upVotes: payload.updatedSolution.upVotes,
+          downVotes: payload.updatedSolution.downVotes
+        }
+      };
+    case TYPES.UPVOTE_SOLUTION_FAIL:
+    case TYPES.DOWNVOTE_SOLUTION_FAIL:
+      return {
+        ...state,
+        error: payload.error
       };
 
     default:
