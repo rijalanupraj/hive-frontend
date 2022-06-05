6a892c9917fe5c61a12f42a779b7f8be3d99d950
samirthapaliya00 <43093117+samirthapaliya@users.noreply.github.com>
2022-06-05 20:09:04 +0545

    bug fix

--- src/userpages/ViewSolution/components/CommentSection.js
+++ src/userpages/ViewSolution/components/CommentSection.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import * as Yup from 'yup';
-import { useFormik, Form, FormikProvider } from 'formik';
-import { LoadingButton } from '@mui/lab';
-import { Stack, TextField, Typography, Grid } from '@mui/material';
-import { useDispatch } from 'react-redux';
-
+import React from "react";
+import * as Yup from "yup";
+import { useFormik, Form, FormikProvider } from "formik";
+import { LoadingButton } from "@mui/lab";
+import { Stack, TextField, Typography, Grid, Avatar } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
 // Internal Import
-import { addComment } from '../../../redux/actions/viewSolutionActions';
+import { addComment } from "../../../redux/actions/viewSolutionActions";
 
 const CommentSection = ({ solution }) => {
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const auth = useSelector((state) => state.auth);
   const commentSchema = Yup.object().shape({
     text: Yup.string()
-      .required('Comment is required')
-      .min(10, 'Comment must be at least 10 characters long')
-      .max(1000, 'Comment must be less than 1000 characters long')
+      .required("Comment is required")
+      .min(10, "Comment must be at least 10 characters long")
+      .max(1000, "Comment must be less than 1000 characters long"),
   });
 
   const formik = useFormik({
     initialValues: {
-      text: ''
+      text: "",
     },
     validationSchema: commentSchema,
-    onSubmit: values => {
+    onSubmit: (values) => {
       console.log(values);
       dispatch(addComment(solution._id, values));
       formik.resetForm();
-    }
+    },
   });
 
   const { text } = formik.values;
@@ -37,15 +42,40 @@ const CommentSection = ({ solution }) => {
 
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Typography variant='h6'>View Comment</Typography>
+          <Typography variant="h6">View Comment</Typography>
         </Grid>
         <Grid item xs={12}>
-          {solution.comments.map(comment => (
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Typography variant='body1'>{comment.text}</Typography>
-              </Grid>
-            </Grid>
+          {solution.comments.map((comment) => (
+            <Card sx={{ minWidth: 250, mt: 1, color: "#001E3C" }}>
+              <CardContent>
+                <Grid container wrap="nowrap" spacing={2}>
+                  <Grid item>
+                    <Avatar alt="Profile Photo" src={user.profileImage} />
+                  </Grid>
+                  <Grid justifyContent="left" item xs zeroMinWidth>
+                    <h4 style={{ margin: 0, textAlign: "left" }}>
+                      {/* {comment.user} */}
+                      {solution.user.username}
+                    </h4>
+                    <p style={{ textAlign: "left" }}>{comment.text}</p>
+                    <p style={{ textAlign: "left", color: "gray" }}>
+                      {comment.createdAt.split("T")[0]}
+                    </p>
+
+                    {/* <ButtonGroup
+                      variant="contained"
+                      size="small"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      sx={{ m: 1 }}
+                      aria-label="outlined primary button group"
+                    >
+                      <Button>Edit</Button>
+                      <Button classname="btn-delete"oup> */}
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
           ))}
         </Grid>
       </Grid>
@@ -53,40 +83,45 @@ const CommentSection = ({ solution }) => {
       {/* View Comment Ends */}
 
       <Stack spacing={3}>
-        <Typography variant='h6' sx={{ fontWeight: 'bold', my: 2 }} component='div' gutterBottom>
+        <Typography
+          variant="h6"
+          sx={{ fontWeight: "bold", my: 2 }}
+          component="div"
+          gutterBottom
+        >
           Comments ({solution.comments.length})
         </Typography>
         <FormikProvider value={formik}>
           <Form>
             <Stack spacing={3}>
               <TextField
-                name='text'
-                label='Comment'
-                variant='outlined'
+                name="text"
+                label="Comment"
+                variant="outlined"
                 fullWidth
                 multiline
                 rows={4}
                 rowsMax={4}
                 sx={{
                   mb: 3,
-                  '& > *': {
-                    width: '100%'
-                  }
+                  "& > *": {
+                    width: "100%",
+                  },
                 }}
-                {...formik.getFieldProps('text')}
+                {...formik.getFieldProps("text")}
                 helperText={formik.touched.text && formik.errors.text}
                 error={formik.touched.text && !!formik.errors.text}
               />
               <LoadingButton
-                type='submit'
-                variant='contained'
-                color='primary'
+                type="submit"
+                variant="contained"
+                color="primary"
                 fullWidth
                 sx={{
                   mb: 3,
-                  '& > *': {
-                    width: '100%'
-                  }
+                  "& > *": {
+                    width: "100%",
+                  },
                 }}
               >
                 Post
