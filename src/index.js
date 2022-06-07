19b002a776006f23a8c649ccd553655cb36ca991
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-06-08 00:13:52 +0545

    🎨 Improve ask-question and post-solution uiu

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 
+// highlight
+import "./utils/highlight";
+
+// scroll bar
+import "simplebar/src/simplebar.css";
+
+// lightbox
+import "react-image-lightbox/style.css";
+
+// Editor
+import "react-quill/dist/quill.snow.css";
+
 // Internal Import
 import store from "./redux/store";
 
