5de89d2a65f4d41f4e1841e3e05d26a52337db1f
samirthapaliya00 <43093117+samirthapaliya@users.noreply.github.com>
2022-06-16 19:50:28 +0545

    design change of contribution

--- src/sections/user/MyProfile/ProfileContribution.js
+++ src/sections/user/MyProfile/ProfileContribution.js
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
 // @mui
 import { styled } from "@mui/material/styles";
-import { Link, Card, Typography, CardHeader, Stack, Chip } from "@mui/material";
+import {
+  Link,
+  Card,
+  Typography,
+  CardHeader,
+  Stack,
+  Chip,
+  Divider,
+  Paper,
+} from "@mui/material";
 // components
 import Iconify from "../../../components/Iconify";
 
@@ -13,6 +22,13 @@ const IconStyle = styled(Iconify)(({ theme }) => ({
   marginTop: 1,
   flexShrink: 0,
   marginRight: theme.spacing(2),
+}));
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.primary,
 }));
 
 // ----------------------------------------------------------------------
@@ -28,13 +44,27 @@ export default function ProfileContribution({ profile }) {
     <Card>
       <CardHeader title="Contribution" />
 
-      <Stack spacing={2} sx={{ p: 3 }}>
-        <Typography variant="body2">{Contribution}</Typography>
+      <Stack spacing={3} direction="row" sx={{ p: 3 }}>
+        <Item>
+          <Typography variant="body2">{Contribution}</Typography>
+          <Typography variant="h4">
+            {/* {followers ? followers.length : "0"} 0 */}0
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            Questions Asked
+          </Typography>
+        </Item>
+        <Divider orientation="vertical" variant="middle" flexItem />
 
-        <Stack direction="row" spacing={1}>
-          <Chip label="6 solution" size="small" />
-          <Chip color="info" label="5 questions" size="small" />
-        </Stack>
+        <Item>
+          <Typography variant="body2">{Contribution}</Typography>
+          <Typography variant="h4">
+            {/* {followers ? followers.length : "0"} 0 */}0
+          </Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            Solution Provided
+          </Typography>
+        </Item>
       </Stack>
     </Card>
   );
