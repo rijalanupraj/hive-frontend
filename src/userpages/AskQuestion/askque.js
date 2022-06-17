9b2e85cf06ef7660022483f6e5d74150a2644378
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-06-18 01:10:21 +0545

    🎨 Update Ask Question UI

--- src/userpages/AskQuestion/askque.js
+++ src/userpages/AskQuestion/askque.js
@@ -9,6 +9,7 @@ import { useDispatch, useSelector } from "react-redux";
 // @mui
 import { LoadingButton } from "@mui/lab";
 import { styled } from "@mui/material/styles";
+import { useSnackbar } from "notistack";
 import {
   Grid,
   Card,
@@ -19,6 +20,9 @@ import {
   Autocomplete,
   Paper,
   Container,
+  Switch,
+  FormControlLabel,
+  FormGroup
 } from "@mui/material";
 // routes
 // components
@@ -27,7 +31,7 @@ import {
   RHFEditor,
   FormProvider,
   RHFTextField,
-  RHFUploadSingleFile,
+  RHFUploadSingleFile
 } from "../../components/hook-form";
 
 import Page from "../../components/Page";
@@ -38,8 +42,8 @@ import { getAllAvailableTags } from "../../redux/actions/tagActions";
 
 const RootStyle = styled("div")(({ theme }) => ({
   [theme.breakpoints.up("md")]: {
-    display: "flex",
-  },
+    display: "flex"
+  }
 }));
 
 // ----------------------------------------------------------------------
@@ -47,7 +51,7 @@ const RootStyle = styled("div")(({ theme }) => ({
 const LabelStyle = styled(Typography)(({ theme }) => ({
   ...theme.typography.subtitle2,
   color: theme.palette.text.secondary,
-  marginBottom: theme.spacing(1),
+  marginBottom: theme.spacing(1)
 }));
 
 const categoriesList = ["Government", "Health", "Education", "Vechiles"];
@@ -56,9 +60,11 @@ const categoriesList = ["Government", "Health", "Education", "Vechiles"];
 
 export default function AskQuestion() {
   const dispatch = useDispatch();
-  const question = useSelector((state) => state.question);
-  const tags = useSelector((state) => state.tag);
+  const question = useSelector(state => state.question);
+  const [showMetaData, setShowMetaData] = useState(false);
+  const tags = useSelector(state => state.tag);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
   const tagsList = tags.tagsList;
 
@@ -71,28 +77,25 @@ export default function AskQuestion() {
   const NewQuestionSchema = Yup.object().shape({
     title: Yup.string().required("Title is required"),
     description: Yup.string().required("Description is required"),
-    content: Yup.string().min(100).required("Content is required"),
-    category: Yup.string().min(3).required("Category is required"),
-    cover: Yup.mixed(),
+    category: Yup.string().required("Category is required"),
+    tags: Yup.array().required("Tags is required").min(1, "Tags is required")
   });
 
   const defaultValues = {
     title: "",
     description: "",
-    content: "",
-    cover: null,
     category: "",
-    tags: ["Logan"],
+    tags: [],
     publish: true,
     comments: true,
     metaTitle: "",
     metaDescription: "",
-    metaKeywords: ["Logan"],
+    metaKeywords: []
   };
 
   const methods = useForm({
     resolver: yupResolver(NewQuestionSchema),
-    defaultValues,
+    defaultValues
   });
 
   const {
@@ -101,171 +104,137 @@ export default function AskQuestion() {
     control,
     setValue,
     handleSubmit,
-    formState: { isSubmitting, isValid },
+    formState: { isSubmitting, isValid }
   } = methods;
 
   const values = watch();
 
   const onSubmit = async () => {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 500));
       dispatch(askQuestion(values, navigate));
       reset();
-      // enqueueSnackbar("Post success!");
+      enqueueSnackbar("Question asked successfully", {
+        variant: "success"
+      });
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleDrop = useCallback(
-    (acceptedFiles) => {
-      const file = acceptedFiles[0];
-
-      if (file) {
-        setValue(
-          "cover",
-          Object.assign(file, {
-            preview: URL.createObjectURL(file),
-          })
-        );
-      }
-    },
-    [setValue]
-  );
-
   return (
-    <Page title="Ask Question">
-      <Container component="main">
-        <Paper variant="outlined" sx={{ my: { xs: 3} }}>
+    <Page title='Ask Question'>
+      <Container component='main'>
+        <Paper variant='outlined' sx={{ my: { xs: 3 }, p: { xs: 3 } }}>
           <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
             <Grid container spacing={3}>
               <Grid item xs={12} md={8}>
-                <Card
-                  sx={{ p: 3 }}
-                >
+                <Card sx={{ p: 3 }}>
                   <Stack spacing={3}>
-                    <RHFTextField name="title" label="Question Title" />
+                    <RHFTextField name='title' label='Question Title' />
 
-                    <RHFTextField
-                      name="description"
-                      label="Description"
-                      multiline
-                      rows={3}
-                    />
-
-                    <div>
-                      <LabelStyle>Content</LabelStyle>
-                      <RHFEditor name="content" />
-                    </div>
-
-                    <div>
-                      <LabelStyle>Cover</LabelStyle>
-                      <RHFUploadSingleFile
-                        name="cover"
-                        accept="image/*"
-                        maxSize={3145728}
-                        onDrop={handleDrop}
-                      />
-                    </div>
+                    <RHFTextField name='description' label='Description' multiline rows={5} />
                   </Stack>
                 </Card>
               </Grid>
 
               <Grid item xs={12} md={4}>
-                <Card
-                  sx={{ p: 3 }}
-                >
+                <Card sx={{ p: 3 }}>
                   <Stack spacing={3}>
                     <Controller
-                      name="category"
+                      name='category'
                       control={control}
                       render={({ field }) => (
                         <Autocomplete
                           freeSolo
-                          onChange={(event, newValue) =>
-                            field.onChange(newValue)
-                          }
-                          options={categoriesList.map((option) => option)}
-                          renderInput={(params) => (
-                            <TextField label="Category" {...params} />
-                          )}
+                          onChange={(event, newValue) => field.onChange(newValue)}
+                          options={categoriesList.map(option => option)}
+                          renderInput={params => <TextField label='Category' {...params} />}
                         />
                       )}
                     />
                     <Controller
-                      name="tags"
+                      name='tags'
                       control={control}
                       render={({ field }) => (
                         <Autocomplete
                           multiple
                           freeSolo
-                          onChange={(event, newValue) =>
-                            field.onChange(newValue)
-                          }
-                          options={tagsList.map((option) => option)}
+                          onChange={(event, newValue) => field.onChange(newValue)}
+                          options={tagsList.map(option => option)}
                           renderTags={(value, getTagProps) =>
                             value.map((option, index) => (
                               <Chip
                                 {...getTagProps({ index })}
                                 key={option}
-                                size="small"
+                                size='small'
                                 label={option}
                               />
                             ))
                           }
-                          renderInput={(params) => (
-                            <TextField label="Tags" {...params} />
-                          )}
+                          renderInput={params => <TextField label='Tags' {...params} />}
                         />
                       )}
                     />
+                    <FormGroup>
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            value={showMetaData}
+                            onChange={() => setShowMetaData(!showMetaData)}
+                          />
+                        }
+                        label='Meta Data'
+                      />
+                    </FormGroup>
 
-                    <RHFTextField name="metaTitle" label="Meta title" />
+                    {showMetaData && (
+                      <>
+                        <RHFTextField name='metaTitle' label='Meta title' />
 
-                    <RHFTextField
-                      name="metaDescription"
-                      label="Meta description"
-                      fullWidth
-                      multiline
-                      rows={3}
-                    />
+                        <RHFTextField
+                          name='metaDescription'
+                          label='Meta description'
+                          fullWidth
+                          multiline
+                          rows={3}
+                        />
 
-                    <Controller
-                      name="metaKeywords"
-                      control={control}
-                      render={({ field }) => (
-                        <Autocomplete
-                          multiple
-                          freeSolo
-                          onChange={(event, newValue) =>
-                            field.onChange(newValue)
-                          }
-                          options={tagsList.map((option) => option)}
-                          renderTags={(value, getTagProps) =>
-                            value.map((option, index) => (
-                              <Chip
-                                {...getTagProps({ index })}
-                                key={option}
-                                size="small"
-                                label={option}
-                              />
-                            ))
-                          }
-                          renderInput={(params) => (
-                            <TextField label="Meta keywords" {...params} />
+                        <Controller
+                          name='metaKeywords'
+                          control={control}
+                          render={({ field }) => (
+                            <Autocomplete
+                              multiple
+                              freeSolo
+                              onChange={(event, newValue) => field.onChange(newValue)}
+                              options={tagsList.map(option => option)}
+                              renderTags={(value, getTagProps) =>
+                                value.map((option, index) => (
+                                  <Chip
+                                    {...getTagProps({ index })}
+                                    key={option}
+                                    size='small'
+                                    label={option}
+                                  />
+                                ))
+                              }
+                              renderInput={params => (
+                                <TextField label='Meta keywords' {...params} />
+                              )}
+                            />
                           )}
                         />
-                      )}
-                    />
+                      </>
+                    )}
                   </Stack>
                 </Card>
 
-                <Stack direction="row" spacing={1.5} sx={{ mt: 3 }}>
+                <Stack direction='row' spacing={1.5} sx={{ mt: 3 }}>
                   <LoadingButton
                     fullWidth
-                    type="submit"
-                    variant="contained"
-                    size="large"
+                    type='submit'
+                    variant='contained'
+                    size='large'
                     loading={isSubmitting}
                   >
                     Ask Question
