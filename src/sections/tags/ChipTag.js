2983d876e54a3fac08ea495d1a341372f661b97d
samirthapaliya00 <43093117+samirthapaliya@users.noreply.github.com>
2022-07-02 16:48:52 +0545

    :+1: some changes in chip

---
+++ src/sections/tags/ChipTag.js
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { capitalCase } from "change-case";
+
+// @mui
+import {
+  Box,
+  Grid,
+  Card,
+  Button,
+  Typography,
+  InputAdornment,
+  Stack,
+  Chip,
+} from "@mui/material";
+
+// components
+import Iconify from "../../components/Iconify";
+import InputStyle from "../../components/InputStyle";
+import SearchNotFound from "../../components/SearchNotFound";
+
+// ----------------------------------------------------------------------
+
+ChipTags.propTypes = {
+  tags: PropTypes.array,
+  findTags: PropTypes.string,
+  onFindTags: PropTypes.func,
+};
+
+export default function ChipTags({ tags, findTags, onFindTags }) {
+  const tagFiltered = applyFilter(tags, findTags);
+
+  const isNotFound = tagFiltered.length === 0;
+
+  return (
+    <Grid container spacing={3}>
+      {tagFiltered.map((tag) => (
+        <Grid key={tag._id} item>
+          <TagCard tag={tag} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+// ----------------------------------------------------------------------
+TagCard.propTypes = {
+  tag: PropTypes.object,
+};
+
+function TagCard({ tag }) {
+  return (
+    <Stack direction="row" spacing={1}>
+      <Chip
+        label={tag}
+        avatar={<Iconify icon={"ic:tag"} width={24} height={24} />}
+        variant="outline"
+        size="small"
+        clickable
+        // sx={{width:'10%'}}
+      >
+        {capitalCase(tag)}
+      </Chip>
+    </Stack>
+  );
+}
+
+function applyFilter(array, query) {
+  if (query) {
+    return array.filter(
+      (tag) => tag.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
+  }
+
+  return array;
+}
