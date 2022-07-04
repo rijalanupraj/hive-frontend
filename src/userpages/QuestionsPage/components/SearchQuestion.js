96664f310bad5592b3271655e3da8a0a1b5b17f9
Shankar4919 <itsme.shankar8@gmail.com>
2022-07-04 15:38:13 +0545

    category in question page

--- src/userpages/QuestionsPage/components/SearchQuestion.js
+++ src/userpages/QuestionsPage/components/SearchQuestion.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { useCallback, useState, useEffect } from "react";
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Box,
   Link,
@@ -13,6 +15,7 @@ import {
   IconButton,
   InputAdornment,
   FormControlLabel,
+  Autocomplete,
   Divider,
   Collapse,
   Button,
@@ -22,8 +25,55 @@ import Image from "../../../components/Image";
 import Iconify from "../../../components/Iconify";
 import MyAvatar from "../../../components/MyAvatar";
 import EmojiPicker from "../../../components/EmojiPicker";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
 
 const SearchQuestion = () => {
+  const category = useSelector((state) => state.category);
+  const [categoriesList, setCategoriesList] = useState([]);
+
+  useEffect(() => {
+    if (category.categoryList) {
+      setCategoriesList(category.categoryList);
+    }
+  }, [category.categoryList]);
+
+  const NewQuestionSchema = Yup.object().shape({
+    title: Yup.string()
+      .required("Title is required")
+      .min(15, "Title must be at least 15 characters")
+      .max(400, "Title must be less than 400 characters"),
+    description: Yup.string(),
+    category: Yup.string().required("Category is required"),
+    tags: Yup.array().required("Tags is required").min(1, "Tags is required"),
+  });
+
+  const defaultValues = {
+    title: "",
+    description: "",
+    category: "",
+    tags: [],
+    publish: true,
+    metaTitle: "",
+    metaDescription: "",
+    metaKeywords: [],
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(NewQuestionSchema),
+    defaultValues,
+  });
+  
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting, isValid, errors },
+  } = methods;
   return (
     <>
       <Paper>
@@ -36,7 +86,7 @@ const SearchQuestion = () => {
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <Button size="medium" variant="outlined" >
+                  <Button size="medium" variant="outlined">
                     Search
                   </Button>
                 </InputAdornment>
@@ -52,6 +102,26 @@ const SearchQuestion = () => {
               },
             }}
           />
+          
+          <Controller
+            name="category"
+            control={control}
+            render={({ field }) => (
+              <Autocomplete
+                onChange={(event, newValue) => field.onChange(newValue)}
+                options={categoriesList.map((option) => option.title)}
+                renderInput={(params) => (
+                  <TextField
+                    label="Category"
+                    {...params}
+                    error={errors.category}
+                    helperText={errors.category?.message}
+                  />
+                )}
+              />
+            )}
+          />
+
           {/* <IconButton>
             <Iconify icon={"charm:search"} width={24} height={24} />
           </IconButton> */}
