4cdc03b87107fadf72dfef7e60fcbb52fdd9cea4
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-07-08 23:28:00 +0545

    🎨 Hot Question & User Level Gamification

--- src/sections/user/MyProfile/UserXpLevel.js
+++ src/sections/user/MyProfile/UserXpLevel.js
@@ -1,19 +1,25 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 // @mui
-import { Card, CardHeader, Typography, Stack, LinearProgress } from '@mui/material';
+import {
+  Card,
+  CardHeader,
+  Typography,
+  Stack,
+  LinearProgress,
+} from "@mui/material";
 // utils
-import { fPercent, fCurrency } from '../../../utils/formatNumber';
+import { fPercent, fCurrency } from "../../../utils/formatNumber";
 // _mock_
-import { _ecommerceSalesOverview } from '../../../_mock';
+import { _ecommerceSalesOverview } from "../../../_mock";
 
 // ----------------------------------------------------------------------
 
-export default function UserXpLevelview() {
+export default function UserXpLevelview({ profile }) {
   return (
     <Card>
-      <CardHeader title="Level 1" />
-      <Stack spacing={4} sx={{ p: 3 }}> 
-          <ProgressItem />
+      <CardHeader title={`Level ${profile.XPLevel}`} />
+      <Stack spacing={4} sx={{ p: 3 }}>
+        <ProgressItem profile={profile} />
       </Stack>
     </Card>
   );
@@ -21,22 +27,26 @@ export default function UserXpLevelview() {
 
 // ----------------------------------------------------------------------
 
-function ProgressItem() {
+function ProgressItem({ profile }) {
+  let point =
+    profile.XPpoints < 100 ? profile.XPpoints : profile.XPpoints / 100;
+  console.log(point);
+
   return (
     <Stack spacing={1}>
       <Stack direction="row" alignItems="center">
         <Typography variant="subtitle2" sx={{ flexGrow: 1 }}>
           Progress
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          74%
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {fPercent(point)}
         </Typography>
       </Stack>
 
       <LinearProgress
         variant="determinate"
-        value="74"
-        color='success'
+        value={point}
+        color="success"
         style={{
           height: 8,
         }}
