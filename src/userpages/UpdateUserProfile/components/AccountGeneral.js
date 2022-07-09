b38d3d1421c4ceee4e4f6a4573a8e08044841670
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-07-09 11:33:53 +0545

    change profile picture

--- src/userpages/UpdateUserProfile/components/AccountGeneral.js
+++ src/userpages/UpdateUserProfile/components/AccountGeneral.js
@@ -1,10 +1,19 @@
 // External Dependencies
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import * as Yup from "yup";
-import { Box, Grid, Card, Stack, Typography, TextField } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Card,
+  Stack,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { useFormik, Form, FormikProvider } from "formik";
 import { useDispatch, useSelector } from "react-redux";
+import { useSnackbar } from "notistack";
 
 // utils
 import { fData } from "../../../utils/formatNumber";
@@ -16,14 +25,20 @@ import {
   RHFTextField,
   RHFUploadAvatar,
 } from "../../../components/hook-form";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 // Internal Import
-import { updateProfile } from "../../../redux/actions/userActions";
+import {
+  updateProfile,
+  updateProfileImage,
+} from "../../../redux/actions/userActions";
 import {
   UploadAvatar,
   UploadMultiFile,
   UploadSingleFile,
 } from "../../../components/upload";
+import { useMemo } from "react";
 
 // ----------------------------------------------------------------------
 
@@ -31,6 +46,8 @@ export default function AccountGeneral() {
   const auth = useSelector((state) => state.auth);
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
+  const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
     if (!user.isLoading) {
@@ -48,6 +65,55 @@ export default function AccountGeneral() {
     about: Yup.string(),
   });
 
+  const UpdateImageSchema = Yup.object().shape({
+    profileImage: Yup.mixed().test(
+      "required",
+      "Profile Image is required",
+      (value) => value !== ""
+    ),
+  });
+
+  const imageDefaultValues = useMemo(
+    () => ({
+      profileImage: auth?.me?.profilePhoto?.url,
+    }),
+    [auth.me]
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(UpdateImageSchema),
+    imageDefaultValues,
+  });
+
+  const profileValues = methods.watch();
+
+  useEffect(() => {
+    if (auth.me) {
+      methods.reset(imageDefaultValues);
+    }
+    if (!auth.me.profilePhoto) {
+      methods.reset(imageDefaultValues);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [auth.me]);
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      if (file) {
+        methods.setValue(
+          "profileImage",
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        );
+        setShowButton(true);
+      }
+    },
+    [methods.setValue]
+  );
+
   const formik = useFormik({
     initialValues: {
       name: auth.me.name || "",
@@ -56,9 +122,17 @@ export default function AccountGeneral() {
     },
     validationSchema: UpdateProfileSchema,
     onSubmit: (values) => {
-      dispatch(updateProfile(formik.values));
+      dispatch(updateProfile(formik.values, enqueueSnackbar));
     },
   });
+
+  const onProfileImageSubmit = async () => {
+    console.log("Submitting");
+    const formData = new FormData();
+    formData.append("profileImage", profileValues.profileImage);
+    dispatch(updateProfileImage(formData, enqueueSnackbar));
+    setShowButton(false);
+  };
 
   const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } =
     formik;
@@ -67,9 +141,42 @@ export default function AccountGeneral() {
     <Grid container spacing={3}>
       <Grid item xs={12} md={4}>
         <Card sx={{ py: 10, px: 3, textAlign: "center" }}>
-          <Box sx={{ mb: 5 }}>
+          <FormProvider
+            methods={methods}
+            onSubmit={methods.handleSubmit(onProfileImageSubmit)}
+          >
+            <Box sx={{ mb: 5 }}>
+              <RHFUploadAvatar
+                name="profileImage"
+                accept="image/*"
+                maxSize={3145728}
+                onDrop={handleDrop}
+                helperText={
+                  <Typography
+                    variant="caption"
+                    sx={{
+                      mt: 2,
+                      mx: "auto",
+                      display: "block",
+                      textAlign: "center",
+                      color: "text.secondary",
+                    }}
+                  >
+                    Allowed *.jpeg, *.jpg, *.png, *.gif
+                    <br /> max size of {fData(3145728)}
+                  </Typography>
+                }
+              />
+              {showButton && (
+                <Button type="submit" variant="contained" color="primary">
+                  Save
+                </Button>
+              )}
+            </Box>
+            {/* <Box sx={{ mb: 5 }}>
             <UploadAvatar />
-          </Box>
+          </Box> */}
+          </FormProvider>
         </Card>
       </Grid>
 
